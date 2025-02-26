155b339872bb998e29bbad2251ee3bb51b58744c
Xollll <mzlkrnainn@gmail.com>
2025-02-26 22:14:17 +0800

    gradient update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,26 +23,52 @@ class _HomePageState extends State<HomePage> {
     _deviceHeight = MediaQuery.of(context).size.height; //get the height of the device
     _deviceWidth = MediaQuery.of(context).size.width; //get the width of the device
     return Scaffold(
-      body: Stack(children: <Widget>[_featuredGamesWidget()
+      body: Stack(children: <Widget>[
+        _featuredGamesWidget(), //this is function part
+        _gradientBoxWidget(),
       ],) ,
     );
   }
+
+  //this is the featured games widget part
   Widget _featuredGamesWidget(){
     return SizedBox( //this part is used for dynamically scale to a different screen size and adjust to different screen size
     height: _deviceHeight * 0.50 , 
     width: _deviceWidth , 
     child: PageView(
       scrollDirection: Axis.horizontal, //scrolling horizontally
-      children: featuredGames.map((_game){
+      children: featuredGames.map((game){
         return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: NetworkImage(_game.coverImage.url), //NetworkImage
+          image: NetworkImage(game.coverImage.url), //NetworkImage
           fit: BoxFit.cover,
         ), //DecorationImage
       ), // BoxDecoration
     ); //container
     }).toList(),
     )); //listview // SizedBox
+  }
+
+//this is the gradient box widget part
+  Widget _gradientBoxWidget(){
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+      height: _deviceHeight * 0.80,
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+          Color.fromRGBO(35, 45, 59, 1.0),
+          Colors.transparent,
+          ],
+          stops: [0.65, 1.0],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
   }
 }
